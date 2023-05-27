7f5438240350493ca78ce1e23943a76f9ee3f873
Andrei Stoica <andrei@stoica.dev>
2023-05-27 15:17:59 +0300

    Create basic layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Header } from '@/organisms';
 import './globals.css';
 import { Inter } from 'next/font/google';
 
@@ -14,8 +15,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <div className="fixed inset-0 flex justify-center sm:px-8">
+        <div className="flex w-full max-w-7xl lg:px-8">
+          <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+        </div>
+      </div>
+      <div className="relative">
+        <Header />
+        <main>{children}</main>
+        {/* <Footer /> */}
+      </div>
+    </>
   );
 }
