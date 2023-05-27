23072e00b3dee050390d251059c0dbcea6548d5d
Andrei Stoica <andrei@stoica.dev>
2023-05-27 22:13:32 +0300

    Create stories for Avatar component

---
+++ src/atoms/Avatar/Avatar.stories.tsx
@@ -0,0 +1,27 @@
+import avatarImage from '../../images/avatar.jpg';
+import { Avatar } from './Avatar';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Avatar> = {
+  title: 'Atoms/Avatar',
+  component: Avatar,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof Avatar>;
+
+export const Default: Story = {
+  args: {
+    src: avatarImage,
+    alt: 'Avatar',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    src: avatarImage,
+    alt: 'Avatar',
+    size: 'large',
+  },
+};
