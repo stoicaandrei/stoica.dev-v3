9524a7c0b0278f096f42563e1bf131d1d7744333
Andrei Stoica <andrei@stoica.dev>
2023-05-28 00:16:43 +0300

    Add back hover for icons

--- src/atoms/Icon/Icon.tsx
+++ src/atoms/Icon/Icon.tsx
@@ -11,18 +11,14 @@ export const Icon = ({
     'h-6 w-6 transition',
     {
       'cursor-pointer': clickable,
-      // 'fill-zinc-500 dark:fill-zinc-400': !outlined,
+
       'fill-zinc-500': !outlined,
       'dark:fill-zinc-400': !outlined,
+      'hover:fill-zinc-600': !outlined && clickable,
+      'dark:hover:fill-zinc-300': !outlined && clickable,
+
       'fill-zinc-100 stroke-zinc-500': outlined,
       'dark:fill-zinc-700 dark:stroke-zinc-500': outlined,
-      // 'hover:fill-zinc-600 dark:hover:fill-zinc-300':
-      //   clickable && !revertColors,
-
-      // 'dark:fill-zinc-700 dark:stroke-zinc-500 fill-zinc-100 stroke-zinc-500':
-      //   revertColors,
-      // 'hover:fill-zinc-200 dark:hover:fill-zinc-400 dark:hover:stroke-zinc-400':
-      //   clickable && revertColors,
     },
     className
   );
