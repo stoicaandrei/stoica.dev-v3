58c9ba371c05e914a96cba3921e0a14159370cc1
Andrei Stoica <andrei@stoica.dev>
2023-05-27 14:40:25 +0300

    Define head for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import Head from 'next/head';
+
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <Head>
+        <title>Andrei Stoica - Software engineer, founder, consultant</title>
+        <meta
+          name="description"
+          content="Highly motivated and very passionate React Full Stack Developer 5 years of experience in Typescript, React, Next.js, Tailwind CSS, Ant Design, Node js, PostgreSQL, MongoDB"
+        />
+      </Head>
+    </>
+  );
 }
